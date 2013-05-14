328c078496141e2392ff432ba02d8194fb6980d0
Lars <larstendam@gmail.com>
2013-05-14 12:37:04 +0200

    added Task class

---
+++ src/saxion/persistent/luministodo/wrapper/Task.java
@@ -0,0 +1,19 @@
+package saxion.persistent.luministodo.wrapper;
+
+public class Task {
+	String title;
+	
+	public Task(String title)
+	{
+		this.title = title;
+	}
+	
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	public String getTitle()
+	{
+		return this.title;
+	}
+}
